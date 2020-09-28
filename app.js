908db27d39da30f64805f490298a0c7884d0202f
Ridho Indra Kriswiyanto <41983915+ridhoindra@users.noreply.github.com>
2020-09-28 10:29:53 +0700

    Template App.js & DbConnection (#6)
    * app.js
    
    * app.js
    
    * Template app.js
    
    Co-authored-by: Muhammadkhafid <60431486+Muhammadkhafid@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//ini app.js
+
+const express = require('express')
+const app = express()
+require ('./models/dbConnect')
+const bodyParser = require("body-parser");
+var session = require('express-session')
+const exampleRouter = require('./controllers/example.Controller');
+const exampleUtils = require('./utils/example.Utils')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs');
+app.use(express.static('views'));
+
+app.use(session({
+    secret: 'secrett',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// app.use('/', exampleRouter);
+// app.use('/admin',exampleUtils,exampleRouter);
+
+app.listen(process.env.PORT || 3000,() => {
+    console.log(`App Started on PORT ${process.env.PORT || 3000}`);
+});
---
+++ models/dbConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise
+
+module.exports = mongoose.connect('mongodb+srv://team2:5MwItjLEhPHWsEiz@percobaan.alaon.mongodb.net/team2?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('Connected to DB');
+}).catch(err =>{
+    console.log('Something wrong with the connection');
+    process.exit()
+})
