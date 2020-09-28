9f139d95ef67a374b23ead833b888ee06de46acb
restidiva <48396521+restidiva@users.noreply.github.com>
2020-09-29 06:16:57 +0700

    lanjutan view table produk (#10)
    * Update README.md (#4)
    
    * view tabel produk
    
    * view table produk
    
    Co-authored-by: Ridho Indra Kriswiyanto <41983915+ridhoindra@users.noreply.github.com>
    Co-authored-by: restidivaa <resti_diva_27rpl@student.smktelkom-mlg.sch.id>

--- controllers/example.Controller.js
+++ controllers/example.Controller.js
@@ -1,2 +1,11 @@
 // untuk yang terbiasa pakai controller 
 // untuk yang terbiasa pakai controller dan router
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('login')
+})
+
+module.exports = router
