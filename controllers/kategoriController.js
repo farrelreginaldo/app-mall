c84a0a81be3cc7da23ef55b1b488f4f3103febd4
unknown <muhammad_farrel_27rpl@student.smktelkom-mlg.sch.id>
2020-10-05 08:05:38 +0700

    editKategori.hbs

--- controllers/kategoriController.js
+++ controllers/kategoriController.js
@@ -5,7 +5,9 @@ const Kategori = require('../models/kategoriProduk')
 router.get('/',async (req,res)=>{
     try{
         const kategori = await Kategori.find()
-        res.status(200).json({message: 'Sukses',data:kategori})
+  res.render('kategori',{
+  data:kategori}
+  )
     }catch(err){
         res.status(400).json({message: 'error', error: err.message});
     }
@@ -18,25 +20,49 @@ router.post('/add',async (req,res)=>{
 
     try{
         const tambahKategori = await kategori.save()
-        res.status(200).json({message: 'Berhasil',data: tambahKategori})
+        res.redirect('/kategori')
     }catch(err){
         res.status(400).json({message: 'error', error: err.message})
     }
 })
 
-router.put("/edit/:id", async(req, res) => {
+//router.get("/edit/:id", async(req, res) => {
+  //  try {
+    //    const kategori = await Kategori.find({_id: req.params.id, active:true})
+      //  res.render("editKategori", { data: kategori });
+    //} catch (err) {
+    //    res.status(400).json({message: 'error', error: err.message});
+   // }
+//})
+
+
+
+router.get("/edit/:id", async(req, res) => {
     try {
-        const editKategori = await Produk.findByIdAndUpdate({_id: req.params.id},req.body)
-        res.status(200).json({ message: "Berhasil Mengubah Data kategori", data: editKategori});
+        const kategori = await Kategori.find({_id: req.params.id, active:true})
+        res.render("editKategori", { data: kategori });
+    } catch (err) {
+        res.status(400).json({message: 'error', error: err.message});
+    }
+})
+
+router.post("/update/:id", async(req, res) => {
+    try {
+        const editKategori = await Kategori.findByIdAndUpdate({_id: req.params.id, active:true},req.body)
+        // res.json({ message: "Berhasil Mengubah Data Distributor", data: editProduk});
+        res.redirect('/kategori')
     } catch (err) {
         res.status(400).json({message: 'error', error: err.message});
     }
 });
 
-router.delete("/delete/:id", async(req, res) => {
+
+
+
+router.get("/delete/:id", async(req, res) => {
     try {
-        await Produk.deleteOne({_id:req.params.id});
-        res.status(200).json({ message: "Berhasil Menghapus Data kategori" });
+        await Kategori.deleteOne({_id:req.params.id});
+        res.redirect('/kategori')
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
